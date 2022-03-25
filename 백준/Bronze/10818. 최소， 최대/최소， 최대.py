08641e85f0f484c524bc5de6aac837478a7d2892
Nicholas Yoo <sodlalwl13@gmail.com>
2022-03-26 01:09:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 548 ms, Memory: 148416 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = list(map(int, input().split()))
+max = A[0]
+min = A[0]
+for i in range(N):
+    if A[i] > max:
+        max = A[i]
+    if A[i] < min:
+        min = A[i]
+print(min, max)
