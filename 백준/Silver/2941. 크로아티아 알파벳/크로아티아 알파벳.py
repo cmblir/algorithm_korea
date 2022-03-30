152f9efb0879133bf4a3157267c123d7d3c7bd99
Nicholas Yoo <sodlalwl13@gmail.com>
2022-03-31 01:10:41 +0900

    Update 크로아티아 알파벳.py

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,8 @@
-N = str(input())
+N = str(input()) # 입력값
 
-cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z='] # 크로아티아 문자
 
-for i in cro:
-    N = N.replace(i, '*')
+for i in cro: # 크로아티아 문자 내에 있으면
+    N = N.replace(i, '*') # 만약 N에 크로아티아 문자가 있으면 N문자에 덮붙여서 문제 해결한다.
 
 print(len(N))
