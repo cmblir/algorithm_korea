67a215618d7e7db19fb0ca68dc086b53c890ea3b
Nicholas Yoo <sodlalwl13@gmail.com>
2022-04-02 23:08:11 +0900

    Update 설탕 배달.py

--- 백준/Bronze/2839. 설탕 배달/설탕 배달.py
+++ 백준/Bronze/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,12 @@
 N = int(input()) # 설탕 킬로그램수
 
 bag = 0
-while N >= 0:
-    if N % 5 == 0:
-        bag += N // 5
+while N >= 0: # N값이 0보다 크다면
+    if N % 5 == 0: # N을 5로 나눴을 때 0이 나오면
+        bag += N // 5 # N으로 나눈 값을 넣은다.
         print(bag)
         break
-    N -= 3
-    bag += 1
+    N -= 3 # 5로 나눈 나머지값들은 5개 될때까지 3을 빼주고
+    bag += 1 # bag에 3을 뺀만큼 +1씩 더해준다.
 else:
     print(-1)
