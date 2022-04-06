067295783db2658b32a5e5c089875be0598c2135
Nicholas Yoo <sodlalwl13@gmail.com>
2022-04-06 23:52:46 +0900

    [Bronze IV] Title: Which Alien?, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,8 @@
+s = int(input())
+b = int(input())
+if 3 <= s <= 4 and b <= 4:
+    print("TroyMartian")
+if s <= 6 and b >= 2:
+    print("VladSaturnian")
+if s <=2 and b <= 3:
+    print("GraemeMercurian")
