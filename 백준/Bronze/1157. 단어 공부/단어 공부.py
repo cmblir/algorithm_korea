ab124be34a24df0d3110c4fc2980677234f0ab64
Nicholas Yoo <sodlalwl13@gmail.com>
2022-05-09 20:04:57 +0900

    [Bronze I] Title: 단어 공부, Time: 268 ms, Memory: 32796 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,11 +1,19 @@
-A = list(input().lower()) # 받은 값을 모두 소문자로
-B = list(set(A)) # 중복값 제거
-tmp = [] # 각 문자열 카운트해서 넣을 리스트
-for i in B: # 중복값이 제거된 문자열의 문자들
-    tmp.append(A.count(i)) # 받은 값에서 중복값을 제거한 문자 카운트세서 리스트에 넣기
+string = input().upper()
+cnt = {}
 
-if tmp.count(max(tmp)) > 1: # 리스트에서 가장 큰 값 개수가 1보다 많은 경우
-    print("?")
-else:
-    max_index = tmp.index(max(tmp)) # 리스트에서 가장 큰 값이 있는 리스트의 문자를 정의
-    print(B[max_index].upper()) # 중복값이 제거된 리스트에서 가장 큰 값을 가진 문자를 대문자로 출력
+for i in string:
+  if i in cnt :
+    cnt[i] += 1
+  else :
+    cnt[i] = 1
+
+val = []
+cnt_max = max(cnt.values())
+for j, k in cnt.items():
+  if k == cnt_max:
+    val.append(j)
+
+if len(val) > 1:
+  print("?")
+else :
+  print("".join(val))
