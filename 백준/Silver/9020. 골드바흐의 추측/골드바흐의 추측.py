22d5bdbc59aca1cb62288de6000a7448082797e1
Nicholas Yoo <sodlalwl13@gmail.com>
2022-05-10 22:24:54 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+# 에라토스테네스의 체 소수 판별 알고리즘 사용
+arr = [1] * 10002
+arr[0] = 0
+arr[1] = 0
+for i in range(2, 101): # 2부터 10000은 100의 제곱이므로 100
+    if arr[i]:
+        for j in range(i + i, 10001, i): # 이전에서 사용한 2의 배수, 3의 배수 등등 소수인 본인을 제외한 모든 배수는 0
+            arr[j] = 0
+
+for _ in range(T):
+    n = int(input())
+    # 이진 탐색 알고리즘 사용
+    rf_m = n // 2 # 중간값이면서 좌측값
+    lf_m = rf_m # 중간값이면서 우측값
+    for _ in range(10000):
+        if arr[rf_m] and arr[lf_m]:
+            print(rf_m, lf_m)
+            break
+        rf_m -= 1 # 우측값 1씩 감소
+        lf_m += 1 # 좌측값 1씩 증가
