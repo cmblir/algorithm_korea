ad30a37ea42a1a3577408a9405a4817af4a91d43
Nicholas Yoo <sodlalwl13@gmail.com>
2022-07-12 01:46:57 +0900

    [Bronze IV] Title: 세수정렬, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.py
+++ 백준/Bronze/2752. 세수정렬/세수정렬.py
@@ -1,10 +1,2 @@
 A = list(map(int, input().split()))
-def num(A):
-    n = len(A)
-    for i in range(n):
-        for j in range(i+1, n):
-            if A[i] > A[j]:
-                A[i], A[j] = A[j], A[i]
-    return A
-
-print(*num(A))
+for i in sorted(A): print(i, end = " ")
