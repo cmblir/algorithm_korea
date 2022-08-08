27cb30f9ec51c8acefde635385524687597119d4
Nicholas Yoo <sodlalwl13@gmail.com>
2022-08-08 23:50:02 +0900

    [level 2] Title: 피보나치 수, Time: 79.31 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,5 @@
-def solution(n):
-    answer = []
-    for i in range(n+1):
-        if i==0 or i==1:
-            answer.append(i)
-        else:
-            f = answer[i-1] + answer[i-2]
-            answer.append(f % 1234567)
-
-    return answer[-1]
+def solution(num):
+    a,b = 0,1
+    for i in range(num):
+        a,b = b,a+b
+    return a % 1234567
