e7c4d747c3532a7499664071382c80ec497d5ba0
Nicholas Yoo <sodlalwl13@gmail.com>
2023-03-09 22:33:16 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.py
@@ -1,12 +1,6 @@
 def solution(spell, dic):
-    result = 0
+    spell = set(spell)
     for idx in dic:
-        spell = {string: 0 for string in spell}
-        for i in idx:
-            if i in spell.keys(): spell[i] += 1
-        cnt = 0
-        for j in spell.values():
-            if j >= 1: cnt += 1
-        if cnt >= len(list(spell.keys())) : result += 1
-    if result >= 1: return 1
-    else: return 2
+        if not spell-set(idx):
+            return 1
+    return 2
