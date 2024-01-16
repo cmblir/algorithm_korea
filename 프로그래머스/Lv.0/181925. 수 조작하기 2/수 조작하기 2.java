e84c0142d545054544352e35500e149cca6abcb5
Nicholas Yoo <sodlalwl13@gmail.com>
2024-01-16 14:42:46 +0900

    [level Lv.0] Title: 수 조작하기 2, Time: 629.00 ms, Memory: 383 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/Lv.0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,20 +1,23 @@
 class Solution {
     public String solution(int[] numLog) {
-        int beforeNumber = numLog[0];
         String answer = "";
         for (int i = 1; i < numLog.length; i++) {
-            int afterNumber = numLog[i];
-            if (beforeNumber + 1 == afterNumber) {
-                answer += "w";
-            } else if (beforeNumber - 1 == afterNumber) {
-                answer += "s";
-            } else if (beforeNumber + 10 == afterNumber) {
-                answer += "d";
-            } else if (beforeNumber - 10 == afterNumber) {
-                answer += "a";
+            int gap = numLog[i] - numLog[i - 1];
+            switch(gap) {
+            case 1 : 
+                    answer += "w";
+                    break;
+            case -1 :
+                    answer += "s";
+                    break;
+            case 10 : 
+                    answer += "d";
+                    break;
+            case -10 : 
+                    answer += "a";
+                    break;
             }
-            beforeNumber = afterNumber;
-        }
-        return answer;
+       }
+    return answer;
     }
 }
