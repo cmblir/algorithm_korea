bef21b4eb15e922cf96da6efe16c9146a9cd45e9
Nicholas Yoo <sodlalwl13@gmail.com>
2024-01-20 01:07:26 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,12 +1,8 @@
 class Solution {
     public String[] solution(String[] names) {
-        if (names.length < 6) {
-            String[] answer = {names[0]};
-            return answer;
-        }
-        double lng = Math.ceil((double)names.length / 5);
-        String[] answer = new String[(int)lng];
-        for (int i = 0, j = 0; i < answer.length; i++, j += 5) {
+        int range = names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1;
+        String[] answer = new String[range];
+        for (int i = 0, j = 0; j < names.length; i++, j += 5) {
             answer[i] = names[j];
         }
         return answer;
