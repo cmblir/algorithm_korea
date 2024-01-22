04d2f4bb961a328f5ae1cfbcb13ad2faf3274140
Nicholas Yoo <sodlalwl13@gmail.com>
2024-01-22 22:55:30 +0900

    [level 0] Title: 커피 심부름, Time: 0.04 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for (String str : order) {
+            if (str.contains("latte")) {
+                answer += 5000;
+            } else if (str.contains("americano")) {
+                answer += 4500;
+            } else {
+                answer += 4500;
+            }
+        }
+        return answer;
+    }
+}
