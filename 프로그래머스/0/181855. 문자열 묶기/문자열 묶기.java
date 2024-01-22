4483bd30b9274ee3444c78be996749301555c564
Nicholas Yoo <sodlalwl13@gmail.com>
2024-01-22 22:35:45 +0900

    [level 0] Title: 문자열 묶기, Time: 5.59 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        int[] arr = new int[10000];
+        for (int i = 0; i < strArr.length; i ++) {
+            arr[strArr[i].length()]++;
+        }
+        return Arrays.stream(arr).max().getAsInt();
+    }
+}
