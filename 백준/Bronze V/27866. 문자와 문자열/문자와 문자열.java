dc03c786716a3d3183a2b3b88cf0748467b8cbd8
Nicholas Yoo <sodlalwl13@gmail.com>
2024-02-14 23:53:48 +0900

    [Bronze V] Title: 문자와 문자열, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze V/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        bw.write(S.charAt(i - 1));
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
