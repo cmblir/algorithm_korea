124472bf29ef9d4939e8165169e33778bd41f866
Nicholas Yoo <sodlalwl13@gmail.com>
2024-02-16 00:01:52 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze V/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze V/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,21 +1,28 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] Student = new int[31];
+        boolean[] Student = new boolean[30];
 
-        for (int i = 1; i <= 28; i++) {
-            Student[Integer.parseInt(br.readLine())]++;
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            Student[n - 1] = true;
         }
 
-        for (int i = 1; i <=30; i++) {
-            if (Student[i] == 0) {
-                System.out.println(i);
+        for (int i = 0; i < Student.length; i++) {
+            if (!Student[i]) {
+                bw.write((i+1) + "\n");
             }
         }
+        br.close();
+        bw.flush();
+        bw.close();
     }
 }
